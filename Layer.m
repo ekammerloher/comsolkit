27979afe2f8268a371813e2f3745669c5a2ba674
kammerloher <kammerloher@ag-bluhm-17.institut2b.physik.rwth-aachen.de>
2015-04-21 04:25:32 +0200

    Add name property and update constructor

--- Layer.m
+++ Layer.m
@@ -2,14 +2,18 @@ classdef Layer
     % Layer Bundles a comsol workplane and an extrude feature into an unit.
     
     properties(Dependent)
+        name % Common name of the workplane and the extrude feature.
         thickness % Thickness of the layer produced by the extrude feature.
         zPosition % z-Position of the layer in the model.
         workPlane % Handle to the workplane feature of Layer.
         extrude % Handle to the extrude feature of Layer.
         selectionTag % Tag of the extrude domain selection feature.
     end
+    properties(Constant)
+        BASE_TAG_WORKPLANE = 'layer_wp'; % Base wp name for uniquetag.
+        BASE_TAG_EXTRUDE = 'layer_ext'; % Base ext name for uniquetag.
+    end
     properties(Access=private)
-        workPlaneTag % Keep the tag to access all workplane features.
         extrudeTag % Access extrude distance in extrude feature.
         hModel % Handle to a ComsolModel object or a derived object.
     end
@@ -20,6 +24,7 @@ classdef Layer
             %
             %  Parameters:
             %  hModel: Required handle to parent ComsolModel type object
+            %  Name: Common name of workpane and the extrude feature.
             %  Thickness: Thickness of layer (must be non-zero, default: 1)
             %  zPosition: z-Position of the layer (default: 0)
             %  %%% when creating from existing extruded workplane %%%
@@ -30,8 +35,8 @@ classdef Layer
             obj.hModel = hModel;
             
             p = inputParser();
+            p.addParameter('Name', '', @ischar);
             p.addParameter('ExtrudeTag', '', @ischar);
-            p.addParameter('WorkPlaneTag', '', @ischar);
             p.addParameter('Thickness', 1, ...
                 @(x) isnumeric(x) && length(x) == 1);
             p.addParameter('zPosition', 0, ...
@@ -39,38 +44,38 @@ classdef Layer
             
             p.parse(varargin{:});
             
-            hasWorkPlaneTag = ~isempty(p.Results.WorkPlaneTag);
-            hasExtrudeTag = ~isempty(p.Results.ExtrudeTag);
-            
-            if xor(hasWorkPlaneTag, hasExtrudeTag)
-            end
-
-            if ~hasWorkPlaneTag
-                obj.workPlaneTag = hModel.geom.uniquetag('layer_wp');
-            else
-                obj.workPlaneTag = p.Results.WorkPlaneTag;
-            end
-            
-            % Setup workplane and use zPosition setter to set quickz.
-            workPlane = hModel.get_or_create(h.Model.geom.feature(), ...
-                                             obj.workPlaneTag, ...
-                                             'WorkPlane');
-            workPlane.set('quickplane', 'xy');
-            obj.zPosition = p.Results.zPosition;
-            %workPlane.set('quickz', p.Results.depth);
-            
-            if ~hasExtrudeTag
+            if isempty(p.Results.ExtrudeTag);
+                workPlaneTag = hModel.geom.uniquetag('layer_wp');
                 obj.extrudeTag = hModel.geom.uniquetag('layer_ext');
+                
+                % Setup workplane.
+                workPlane = hModel.geom.feature().create(workPlaneTag, ...
+                                                         'WorkPlane');
+                workPlane.set('quickplane', 'xy');
+                workPlane.set('quickz', p.Results.zPosition);
+                
+                % Setup extrude.
+                extrude = hModel.geom.feature().create(obj.extrudeTag, ...
+                                                       'Extrude');
+                extrude.set('createselection', 'on');
+                extrude.selection('input').set(workPlaneTag);
+                extrude.set('distance', p.Results.Thickness);
+                
+                % Set common name, if provided.
+                if ~isempty(p.Results.Name)
+                    workPlane.label(p.Results.Name);
+                    extrude.label(p.Results.Name);
+                end
+                % getDoubleArray('distance') to get distance, check for
+                % single value.
+                % selection('input').objects() to get the tag back, check,
+                % that it is one tag.
             else
+                % TODO: Checks on the extrude feature: has workpane
+                obj.extrudeTag = p.Results.ExtrudeTag;
+                obj.extrudeTag = p.Results.ExtrudeTag;
                 obj.extrudeTag = p.Results.ExtrudeTag;
             end
-            
-            % Setup extrude and use thickness setter to set distance.
-            extrude = hModel.get_or_create(h.Model.geom.feature(), ...
-                                             obj.extrudeTag, 'Extrude');
-            extrude.set('createselection', 'on');
-            extrude.selection('input').set(obj.workPlaneTag);
-            obj.thickness = p.Results.Thickness;
         end
     end
 end
